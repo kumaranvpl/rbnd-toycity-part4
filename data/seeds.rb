10ef4d77f44859545744c536be720b84f159a2b7
Kumaran Rajendhiran <kumaranvpl@gmail.com>
2016-03-30 18:29:02 +0000

    Update errors.rb

--- data/seeds.rb
+++ data/seeds.rb
@@ -5,4 +5,13 @@ require 'faker'
 
 def db_seed
   # Your code goes here!
+  data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+  products = []
+  10.times do |id|
+    brand = Faker::Company.name
+    product = Faker::Commerce.product_name
+    price = Faker::Commerce.price
+    products.push(Product.create(brand: brand, name: product, price: price))
+  end
 end
--- lib/errors.rb
+++ lib/errors.rb
@@ -1,1 +1,8 @@
 # Your custom error classes will be placed here
+class ProductNotFoundError
+	class NoSuchProduct < StandardError
+  	end
+
+  	class IdDoesNotExist < StandardError
+  	end
+end
